5c0b19cdc83b32f975377afa7098b9a10a321583
Nicolas Unger <nicolas.unger@unige.ch>
2020-03-06 14:15:13 +0100

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "evidence",
-    version = "0.1.0",
-    author = "Nicolas Unger",
-    author_email = "nicolas.unger@unige.ch",
-    description = "Calculates the bayesian evidence for radial velocity models.",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/nicochunger/evidence",
-    packages = setuptools.find_packages(),
-    classifiers = [
+    name="evidence",
+    version="0.1.1",
+    author="Nicolas Unger",
+    author_email="nicolas.unger@unige.ch",
+    description="Calculates the bayesian evidence for radial velocity models.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nicochunger/evidence",
+    packages=setuptools.find_packages(),
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE",
         "Operating System :: OS Independent",
     ],
-    python_requires = '>=3.6',
-    package_data = {'': ['*.so']}
+    python_requires='>=3.6',
+    package_data={'': ['*.so']}
 )
